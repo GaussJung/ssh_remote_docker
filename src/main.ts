e77225d8e324915ffb6119df49371586ada0c826
GaussJung <cwjung123@gmail.com>
2025-06-25 09:57:29 +0900

    v1.24

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log("========== START Main App 1.22 =============");
+  console.log("========== START Main App 1.24 =============");
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
